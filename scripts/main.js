d12325b15fa3f9929a2de3ce33773321a6944e92
Satendra <satendra.raj1241@gmail.com>
2020-07-23 16:38:19 +0530

    can add , drag , resize

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,9 @@ window.addEventListener('load', () => {
     let pallete = $('#pallete');
     let pHead = $('#pallete-head');
     moveElement(pallete,pHead);
-    
 })
-
-
+let currid=0;
+let is_resize = false;
 function init() {
     if(window.innerWidth<600){
         $('*').css('display','none')
@@ -22,25 +21,50 @@ function init() {
     canvas.height = window.innerHeight
     canvas.width = window.innerWidth
 }
+
 function drawDiv(){
-    $('body').append('<div class="default-div draggable">Text</div>')
-    moveElement($('.default-div'),$('.default-div'));
+    $('body').append('<div class="default-div" id="div-'+currid+'">Text<div class="sizer" id="sizer-'+currid+'"></div></div>');
+    sizeElement($('#div-'+currid+''),$('#sizer-'+currid+''));
+    moveElement($('#div-'+currid+''),$('#div-'+currid+''));
+    currid++;
 }
 function moveElement(element,target){
-    console.log('..')
     target.mousedown((e) => {
         let oldX = e.clientX
         let oldY = e.clientY
         let initX = parseInt(element.css('left').split('px')[0])
         let initY = parseInt(element.css('top').split('px')[0])
         function move(enew) {
-            enew.preventDefault()
-            element.css('left', initX + enew.clientX - oldX)
-            element.css('top', initY + enew.clientY - oldY)
+            if(e.target.className == 'default-div' || e.target.className=='pallete-head') {
+                enew.preventDefault()
+                element.css('left', initX + enew.clientX - oldX)
+                element.css('top', initY + enew.clientY - oldY)
+            }
         }
         document.addEventListener('mousemove', move)
-        target.mouseup((e) => {
+        target.mouseup(() => {
             document.removeEventListener('mousemove', move)
+        })
+    })
+}
+function sizeElement(element,target){
+    target.mousedown((e) => {
+        let oldX = e.clientX
+        let oldY = e.clientY
+        let initW = parseInt(element.css('width').split('px')[0])
+        let initH = parseInt(element.css('height').split('px')[0])
+        function size(enew) {
+            if(e.target.className == 'sizer') {
+                enew.preventDefault()
+            element.css('width', initW + enew.clientX - oldX)
+            element.css('height', initH + enew.clientY - oldY) 
+            }
+            
+            
+        }
+        document.addEventListener('mousemove', size)
+        target.mouseup((e) => {
+            document.removeEventListener('mousemove', size)
         })
     })
 }
